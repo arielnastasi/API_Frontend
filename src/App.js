e66121122914a0a299ecc2ce75ed915179498bbc
Ariel Nastasi <arielnastasi@MacBook-Pro.local>
2020-09-02 19:23:40 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,18 +7,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hola Mundo</h1>
+        <Button color="danger">Hello World</Button>
       </header>
     </div>
   );
