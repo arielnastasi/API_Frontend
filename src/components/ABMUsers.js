34c848f4a7636849fdcc3beb669a2a0afe846a9d
lmitelman <lautaro.mitelman@gmail.com>
2020-09-09 09:29:44 -0300

    9/9

--- src/components/ABMUsers.js
+++ src/components/ABMUsers.js
@@ -2,7 +2,34 @@ import React from 'react';
 
 const ABMUsers = ({ name }) => {
 	return (
-		<h4>ABM users component</h4>
+		<div className="container">
+			<table className="table">
+				<thead>
+					<tr>
+						<th scope="col">Name</th>
+						<th scope="col">ID</th>
+						<th scope="col">Actions</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>German Morone</td>
+						<td>3453453453535</td>
+						<td>3453453453535</td>
+					</tr>
+					<tr>
+						<td>Jacob</td>
+						<td>Thornton</td>
+						<td>@fat</td>
+					</tr>
+					<tr>
+						<td>Larry</td>
+						<td>the Bird</td>
+						<td>@twitter</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
 	);
 }
 
