b7355601764e1e2359c2d565a0db33e4e596959d
BUSTAMJM <bustamante_july@redlink.com.ar>
2020-10-05 19:31:31 -0300

    mejoro logo

--- src/components/Benckmarking.js
+++ src/components/Benckmarking.js
@@ -28,9 +28,6 @@ const Benchmarking = () => {
 		},
 		typo: {
 			fontFamily:  'Lato',
-    		border: 0,
-    		borderRadius: 3,
-    		boxShadow: '0 3px 5px 2px #279daa',
     		color: '#279daa',
     		height: 48,
 			padding: '0 30px',
